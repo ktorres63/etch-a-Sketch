5d19c2345c0e2e8c413c1d8e11baef8e3f539321
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-09 22:12:09 -0500

    Remove pixel2 div

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 const container = document.querySelector(".container");
-let width = 16;
-container.style.width = `${width * 10 + 2}px`; // container width in px
+let size = 16;
+container.style.width = `${size * 10 + 2}px`; // container width in px
 
-const inputSize = document.querySelector("#inputSize");
+const sizeBtn = document.querySelector("#saveSize");
 
-width = parseInt(inputSize.value);
+sizeBtn.addEventListener("click", () => {
+  const inputSize = document.querySelector("#inputSize");
+  size = parseInt(inputSize.value);
+  generateGrid(size)
+});
 
-console.log(typeof width);
-
-for (let i = 0; i < width * width; i++) {
-  let pixel = document.createElement("div");
-  pixel.classList.add("pixel");
-
-  mouseover(pixel);
-
-  container.appendChild(pixel);
+function generateGrid(size){
+  for (let i = 0; i < size * size; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    mouseover(pixel);
+    container.appendChild(pixel);
+  }
 }
+
 
 function mouseover(pixel) {
   pixel.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "black";
-    console.log(e.target);
   });
 }
-
-const pix2 = document.querySelector(".pixel2");
-
-pix2.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "white";
-  console.log(e.target);
-});
