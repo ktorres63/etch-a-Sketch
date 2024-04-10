f70f1ecb438efa586dc96e04d3a2df19fa89f6cc
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-09 19:27:43 -0500

    Add container width from js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+let width = 160;
+container.style.width = `${width+2}px`;
 
 for (let i = 0; i < 256; i++) {
   let pixel = document.createElement("div");
