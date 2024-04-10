3badd4a54dfbcddd837158017e1f8e9771885911
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-09 22:18:41 -0500

    Create a dunction to delete oldpixels

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ container.style.width = `${size * 10 + 2}px`; // container width in px
 const sizeBtn = document.querySelector("#saveSize");
 
 sizeBtn.addEventListener("click", () => {
+
+  deleteGrid(container);
+
   const inputSize = document.querySelector("#inputSize");
   size = parseInt(inputSize.value);
   generateGrid(size)
@@ -18,7 +21,13 @@ function generateGrid(size){
     container.appendChild(pixel);
   }
 }
+function deleteGrid(container){
+  const oldPixels = document.querySelectorAll(".pixel");
+  oldPixels.forEach((px)=>{
+    container.removeChild(px)
+  });
 
+}
 
 function mouseover(pixel) {
   pixel.addEventListener("mouseover", (e) => {
