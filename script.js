e59192c82eb0e7d0156dbeaf9b87a6ad604192b4
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-09 22:05:00 -0500

    Create a function to mouseover event

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const container = document.querySelector(".container");
-let width = 160;
-container.style.width = `${width+2}px`;
+let width = 16;
+container.style.width = `${width * 10 + 2}px`; // container width in px
 
+const inputSize = document.querySelector("#inputSize");
 
+width = parseInt(inputSize.value);
 
+console.log(typeof width);
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < width * width; i++) {
   let pixel = document.createElement("div");
   pixel.classList.add("pixel");
 
-  pixel.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "white"
-    console.log(e.target)
-  });
+  mouseover(pixel);
 
   container.appendChild(pixel);
 }
 
-
-
+function mouseover(pixel) {
+  pixel.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "black";
+    console.log(e.target);
+  });
+}
 
 const pix2 = document.querySelector(".pixel2");
 
 pix2.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "white"
-  console.log(e.target)
-  
+  e.target.style.backgroundColor = "white";
+  console.log(e.target);
 });
