9fa1e41ff50b10464fc83760d1cd8b077177f902
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-10 09:16:25 -0500

    Add reset button

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector("#resetPixels");
+const sizeBtn = document.querySelector("#saveSize");
+
 generateGrid(16);
 
-const sizeBtn = document.querySelector("#saveSize");
+resetBtn.addEventListener("click", () => {
+  const inputSize = document.querySelector("#inputSize");
+  let size = parseInt(inputSize.value);
+  generateGrid(size);
+});
+
 sizeBtn.addEventListener("click", () => {
   const inputSize = document.querySelector("#inputSize");
   let size = parseInt(inputSize.value);
   generateGrid(size);
 });
 
-
-function generateGrid(size){
-  container.style.width = `${size * 10 + 2}px`; // container width in px 
+function generateGrid(size) {
   deletePixels(container);
-  generatePixels(size)
+  container.style.width = `${size * 10 + 2}px`; // container width in px
+  generatePixels(size);
 }
-function generatePixels(size){
+function generatePixels(size) {
   for (let i = 0; i < size * size; i++) {
     let pixel = document.createElement("div");
     pixel.classList.add("pixel");
@@ -22,12 +29,11 @@ function generatePixels(size){
     container.appendChild(pixel);
   }
 }
-function deletePixels(container){
+function deletePixels(container) {
   const oldPixels = document.querySelectorAll(".pixel");
-  oldPixels.forEach((px)=>{
-    container.removeChild(px)
+  oldPixels.forEach((px) => {
+    container.removeChild(px);
   });
-
 }
 
 function mouseover(pixel) {
