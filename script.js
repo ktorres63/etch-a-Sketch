86eff1b199d09b7740ab25a788b13f86aa36fd27
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-10 10:09:16 -0500

    Add functionality to randomColorButton

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+
+
 const baseColor = "rgb(170, 221, 221)";
-const overColor = "black"
+const overColor = "black";
 
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector("#resetPixels");
 const sizeBtn = document.querySelector("#saveSize");
 
-const randomColBtn = document.querySelector("#randomColors");
-
-generateGrid(16);
-
-resetBtn.addEventListener("click", () => {
+function resetPixels() {
   const pixels = document.querySelectorAll(".pixel");
   pixels.forEach((px) => {
     px.style.backgroundColor = baseColor;
   });
+}
+
+const randomColBtn = document.querySelector("#randomColors");
+randomColBtn.addEventListener("click", () => {
+  const inputSize = document.querySelector("#inputSize");
+  let size = parseInt(inputSize.value);
+  generateGrid(size, true);
 });
+
+generateGrid(16);
+
+resetBtn.addEventListener("click", resetPixels);
 
 sizeBtn.addEventListener("click", () => {
   const inputSize = document.querySelector("#inputSize");
@@ -22,39 +31,44 @@ sizeBtn.addEventListener("click", () => {
   generateGrid(size);
 });
 
-function generateGrid(size) {
+function generateGrid(size, randomColor = false) {
   deletePixels(container);
   container.style.width = `${size * 10 + 2}px`; // container width in px
-  generatePixels(size);
+  generatePixels(size, randomColor);
 }
-function generatePixels(size) {
+function generatePixels(size, randomColor = false) {
   for (let i = 0; i < size * size; i++) {
     let pixel = document.createElement("div");
     pixel.classList.add("pixel");
-    mouseover(pixel);
+    mouseover(pixel, randomColor);
     container.appendChild(pixel);
   }
 }
+function getRandomColor() {
+  let randomR = Math.floor(Math.random() * 256);
+  let randomG = Math.floor(Math.random() * 256);
+  let randomB = Math.floor(Math.random() * 256);
+  return `rgb(${randomR},${randomG},${randomB})`;
+  // return "rgb(0,0,100)";
+}
+
+function mouseover(pixel, randomColor = false) {
+  if (randomColor) {
+    pixel.addEventListener("mouseover", (e) => {
+      console.log("Im here")
+      e.target.style.backgroundColor = getRandomColor();
+    });
+  } else {
+    pixel.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = overColor;
+    });
+  }
+}
+
+
 function deletePixels(container) {
   const oldPixels = document.querySelectorAll(".pixel");
   oldPixels.forEach((px) => {
     container.removeChild(px);
   });
-}
-
-function mouseover(pixel, randomColor=false) {
-  if(randomColor){
-
-  }
-  else{
-  pixel.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = overColor;
-  });}
-}
-function randomColor(){
-  let randomR = Math.floor(Math.random()*256);
-  let randomG = Math.floor(Math.random()*256);
-  let randomB = Math.floor(Math.random()*256);
-
-  return `rgb(${randomR},${randomG},${randomB})`;
 }
