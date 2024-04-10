dd16817ae57cd05a9feb9b8323d6d583d6a0c0b9
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-09 19:18:37 -0500

    Create a 16x16 grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   let pixel = document.createElement("div");
   pixel.classList.add("pixel");
   container.appendChild(pixel);
